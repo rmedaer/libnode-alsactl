151f6ced045c955eb5189000bd3d8c94b5af17ca
Raphael Medaer <rme@escaux.com>
2016-03-31 09:43:03 +0200

    [WIP] Commit tests made with enumeration and values of controls.

--- alsactl.cc
+++ alsactl.cc
@@ -2,40 +2,164 @@
 #include <v8.h>
 #include <alsa/asoundlib.h>
 
+#define ALSACTL_SUCCESS 0
+
 using v8::HandleScope;
 using v8::Exception;
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
 using v8::Number;
+using v8::Boolean;
 using v8::Object;
 using v8::String;
 using v8::Value;
 using v8::Array;
+using v8::Null;
 
-void fill_control(Isolate *isolate, Local<Object> obj, snd_hctl_elem_t *elem) {
+
+Local<Array> alsactl_fill_control_items(
+    Isolate *isolate,
+    snd_hctl_elem_t *elem,
+    snd_ctl_elem_info_t *info)
+{
+    Local<Array> items;
+    unsigned int index, count;
+
+    items = Array::New(isolate);
+
+    count = snd_ctl_elem_info_get_items(info);
+    for (index = 0; index < count; index++) {
+        snd_ctl_elem_info_set_item(info, index);
+        if (snd_hctl_elem_info(elem, info) < 0) {
+            items->Set(index, Null(isolate));
+        } else {
+            items->Set(index, String::NewFromUtf8(isolate, snd_ctl_elem_info_get_item_name(info)));
+        }
+    }
+
+    return items;
+}
+
+Local<Value> alsactl_fill_control_value(
+    Isolate *isolate,
+    snd_ctl_elem_type_t type,
+    snd_ctl_elem_value_t *value,
+    unsigned index)
+{
+    switch (type) {
+        case SND_CTL_ELEM_TYPE_NONE:
+            break;
+        case SND_CTL_ELEM_TYPE_BOOLEAN:
+            return Boolean::New(isolate, snd_ctl_elem_value_get_boolean(value, index));
+        case SND_CTL_ELEM_TYPE_INTEGER:
+            return Number::New(isolate, snd_ctl_elem_value_get_integer(value, index));
+        case SND_CTL_ELEM_TYPE_ENUMERATED:
+            return Number::New(isolate, snd_ctl_elem_value_get_enumerated(value, index));   //snd_ctl_elem_info_get_item_name
+            break;
+        case SND_CTL_ELEM_TYPE_BYTES:
+            break;
+        case SND_CTL_ELEM_TYPE_IEC958:
+            break;
+        case SND_CTL_ELEM_TYPE_INTEGER64:
+            return Number::New(isolate, snd_ctl_elem_value_get_integer64(value, index));
+        default:
+            // TODO return ::NULL
+            break;
+    }
+
+    return Null(isolate);
+}
+
+Local<Value> alsactl_wrap_control_value(snd_hctl_elem_t *elem) {
     int err;
-    unsigned int count, i;
-
+    unsigned count, index;
     snd_ctl_elem_type_t type;
     snd_ctl_elem_info_t *info;
-    snd_ctl_elem_value_t *control;
+    snd_ctl_elem_value_t *value;
+    Isolate *isolate;
 
+    // Allocate memory for current control's info and value
     snd_ctl_elem_info_alloca(&info);
-    snd_ctl_elem_value_alloca(&control);
+    snd_ctl_elem_value_alloca(&value);
 
-    // Get infos
+    // Retrieve scope
+    if (!(isolate = Isolate::GetCurrent())) {
+        isolate = Isolate::New();
+        isolate->Enter();
+    }
+
+    // Get control's infos
     if ((err = snd_hctl_elem_info(elem, info)) < 0) {
         isolate->ThrowException(Exception::TypeError(
-            String::NewFromUtf8(isolate, "snd_hctl_elem_info error")));
-        return;
+            String::NewFromUtf8(isolate, "snd_hctl_elem_info error"))); // TODO refactor error
+        return Null(isolate);
     }
+
+    // Number of values
+    count = snd_ctl_elem_info_get_count(info);
+
+    if ((err = snd_hctl_elem_read(elem, value)) < 0) {
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate, "snd_hctl_elem_read error")));
+        return Null(isolate);
+    }
+
+    // Type
+    type = snd_ctl_elem_info_get_type(info);
+
+    if (count <= 1) {
+        return alsactl_fill_control_value(isolate, type, value, 0);
+    } else if (count > 1) {
+        Local<Array> values = Array::New(isolate);
+
+        for (index = 0; index < count; index++) {
+            values->Set(index, alsactl_fill_control_value(isolate, type, value, index));
+        }
+
+        return values;
+    }
+
+    // Ignore the following line, it's a fake for compiler
+    // TODO: find a way to ignore it
+    return Null(isolate);
+}
+
+/**
+ * Export an control from an asound element to a given Node object.
+ * @param {Local<Object>} obj - Node object to fill
+ * @param {snd_hctl_elem_t} elem - Asound element.
+ * @return {Local<Value>} - v8 object or Null
+ */
+Local<Value> alsactl_wrap_control(snd_hctl_elem_t *elem) {
+    int err;
+    Isolate *isolate;
+    snd_ctl_elem_type_t type;
+    snd_ctl_elem_info_t *info;
+    Local<Object> obj;
+
+    // Retrieve scope
+    if (!(isolate = Isolate::GetCurrent())) {
+        isolate = Isolate::New();
+        isolate->Enter();
+    }
+
+    // Allocate memory for current control's info
+    snd_ctl_elem_info_alloca(&info);
+
+    // Get control's infos
+    if ((err = snd_hctl_elem_info(elem, info)) < 0) {
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate, "snd_hctl_elem_info error"))); // TODO refactor error
+        return Null(isolate);
+    }
+
+    // All gonna be OK, instanciate a new v8 object
+    obj = Object::New(isolate);
 
     // Set device name
     obj->Set(String::NewFromUtf8(isolate, "name"),
         String::NewFromUtf8(isolate, snd_ctl_elem_info_get_name(info)));
-
-    count = snd_ctl_elem_info_get_count(info);
 
     // Filter by type
     type = snd_ctl_elem_info_get_type(info);
@@ -55,40 +179,36 @@ void fill_control(Isolate *isolate, Local<Object> obj, snd_hctl_elem_t *elem) {
                 Number::New(isolate, snd_ctl_elem_info_get_step(info)));
             break;
         case SND_CTL_ELEM_TYPE_ENUMERATED:
-            obj->Set(String::NewFromUtf8(isolate, "type"),
-                String::NewFromUtf8(isolate, "enumerated"));
+            obj->Set(String::NewFromUtf8(isolate, "items"),
+                alsactl_fill_control_items(isolate, elem, info));
             break;
         case SND_CTL_ELEM_TYPE_BYTES:
-            obj->Set(String::NewFromUtf8(isolate, "type"),
-                String::NewFromUtf8(isolate, "bytes"));
             break;
         case SND_CTL_ELEM_TYPE_IEC958:
-            obj->Set(String::NewFromUtf8(isolate, "type"),
-                String::NewFromUtf8(isolate, "iec958"));
             break;
         case SND_CTL_ELEM_TYPE_INTEGER64:
-            obj->Set(String::NewFromUtf8(isolate, "type"),
-                String::NewFromUtf8(isolate, "integer64"));
+            obj->Set(String::NewFromUtf8(isolate, "min"),
+                Number::New(isolate, snd_ctl_elem_info_get_min64(info)));
+            obj->Set(String::NewFromUtf8(isolate, "max"),
+                Number::New(isolate, snd_ctl_elem_info_get_max64(info)));
+            obj->Set(String::NewFromUtf8(isolate, "step"),
+                Number::New(isolate, snd_ctl_elem_info_get_step64(info)));
             break;
         default:
-            obj->Set(String::NewFromUtf8(isolate, "type"),
-                String::NewFromUtf8(isolate, "unknown"));
+            // TODO throw error
+            break;
     }
 
-    // if (snd_ctl_elem_info_is_readable(info)) {
-    //     if ((err = snd_hctl_elem_read(elem, control)) < 0) {
-    //         isolate->ThrowException(Exception::TypeError(
-    //             String::NewFromUtf8(isolate, "snd_hctl_elem_read error")));
-    //         return;
-    //     }
-    //
-    //     for (i = 0; i < count; i++) {
-    //
-    //     }
-    // }
+    // Skip export of value if control is not readable
+    if (snd_ctl_elem_info_is_readable(info)) {
+        obj->Set(String::NewFromUtf8(isolate, "value"),
+            alsactl_wrap_control_value(elem));
+    }
+
+    return obj;
 }
 
-void list_controls(const v8::FunctionCallbackInfo<v8::Value>& args) {
+void alsactl_list_elems(const v8::FunctionCallbackInfo<v8::Value>& args) {
     int err;
     int i;
     snd_hctl_t *handle;
@@ -132,10 +252,7 @@ void list_controls(const v8::FunctionCallbackInfo<v8::Value>& args) {
     i = 0;
     for (elem = snd_hctl_first_elem(handle); elem; elem = snd_hctl_elem_next(elem)) {
         // Create a new object
-        Local<Object> obj = Object::New(isolate);
-
-        fill_control(isolate, obj, elem);
-
+        Local<Value> obj = alsactl_wrap_control(elem);
         result_list->Set(i++, obj);
     }
 
@@ -146,7 +263,7 @@ void list_controls(const v8::FunctionCallbackInfo<v8::Value>& args) {
 }
 
 void init(v8::Local<v8::Object> target) {
-    NODE_SET_METHOD(target, "list_controls", list_controls);
+    NODE_SET_METHOD(target, "list_controls", alsactl_list_elems);
 }
 
 NODE_MODULE(alsactl, init);
